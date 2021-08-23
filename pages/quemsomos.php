9f8725a5f1aa7c7a845d58286b425cccca1e6ae5
jul11n <jcbjr28@gmail.com>
2021-08-23 06:08:18 -0300

    alterado o tamanho do logo

--- pages/quemsomos.php
+++ pages/quemsomos.php
@@ -2,19 +2,28 @@
 <h2>Quem Somos</h2>
 <div class="main">
 
-<p> <pre>A cozinha sempre esteve presente em nossas famílias. Comer e fazer doces nos remete a momentos de celebração e bons momentos em nossas vidas.
-A mesa e a comida sempre foram pretexto para nos reunirmos, para estarmos juntos! E éramos em tantos...primos, tios, avós, vizinhos, os meus, 
+
+
+<p> <pre>A cozinha sempre esteve presente em nossas famílias. 
+Comer e fazer doces nos remete a momentos de celebração e bons momentos em nossas vidas.
+A mesa e a comida sempre foram pretexto para nos reunirmos, para estarmos juntos!
+E éramos em tantos...primos, tios, avós, vizinhos, os meus, 
 os seus e os nossos. Tudo junto e misturado. Quanta alegria em torno da mesa. Quantas lembranças boas.
 Foi com o objetivo de compartilhar esses momentos de alegria com outras pessoas que tivemos o desejo de criar uma loja de bolos.
-A partir da primeira ideia, o sonho foi tomando forma e a nossa dedicação em fazer tudo dar certo nos guiou durante a construção desse projeto.
-Buscamos as receitas de família e outras tantas que descobrimos ao longo da vida, em cursos e livros de confeitaria. Foi um processo de profissionalização,
-transformação, testes e adaptação de receitas e uma constante busca pelos melhores ingredientes, até que tudo ficasse com o sabor e a qualidade que desejávamos.
-A LA DE CASA nasce do desejo de levar às pessoas o sabor verdadeiro do bolo caseiro, trazer memória afetiva, paladar cheio de recordações e de descobertas.
+A partir da primeira ideia, o sonho foi tomando forma e a nossa dedicação em fazer tudo 
+dar certo nos guiou durante a construção desse projeto.
+Buscamos as receitas de família e outras tantas que descobrimos ao longo da vida,
+em cursos e livros de confeitaria. Foi um processo de profissionalização,
+transformação, testes e adaptação de receitas e uma constante busca pelos melhores ingredientes,
+até que tudo ficasse com o sabor e a qualidade que desejávamos.
+A LA DE CASA nasce do desejo de levar às pessoas o sabor verdadeiro do bolo caseiro, trazer memória afetiva,
+paladar cheio de recordações e de descobertas.
 O ambiente aconchegante, com a oportunidade de ver a produção dos bolos, tomar um café, um chá, se sentir em casa, na casa da vó.
-O nome LA DE CASA quer transmitir alegria, satisfação, felicidade em forma de bolo. Nada melhor que um café e um bolo LA DE CASA. Queremos que o cheirinho do nosso bolo caseiro chegue à sua casa,
+O nome LA DE CASA quer transmitir alegria, satisfação, felicidade em forma de bolo. Nada melhor que um café e um bolo LA DE CASA.
+Queremos que o cheirinho do nosso bolo caseiro chegue à sua casa,
 te abrace e deixe um gostinho de quero mais. Que possa te trazer as melhores memórias e faça você voltar para apreciar cada vez mais o sabor verdadeiro do bolo caseiro!
 </pre></p>
-<img src="./imagens/logotipo.png" alt="logo-doceria" style="width: 130px;">
+<img src="./imagens/logotipo.png" alt="logo-doceria" style="width: 340px;">
 
 </div>
 
