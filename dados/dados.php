188575209ef7e6ad681224e4c9d31db94ad07bac
jul11n <jcbjr28@gmail.com>
2021-08-22 23:50:36 -0300

    criado o arrey das pagianas

---
+++ dados/dados.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+     
+    </body>
+</html>
+
+<?php
+$produtos = [
+    "bolo" =>[
+      "descricao"=>" Nadinha de misturas prontas ou sabores artificiais,  receitas exclusivas, feitas com ingredientes frescos e muito amor para deixar o seu dia muito especial, são perfeitos para aquele café da tarde em Familia.",
+      'preco'=>16.99,
+      'img'=>'bolo.jpg'
+    ],
+    'cupcake'=>[
+      'descricao'=>"Cupcake no tamanho tradicional (aproximadamente 7cm de diâmetro) com massa nos sabores chocolate, baunilha ou cenoura e coberturas nos sabores ganache de chocolate meio amargo, brigadeiro branco ou preto, doce de leite ou chantilly (que pode ser tingido em cores variadas). Recheios personalizados e confeitos diversos.",
+      'preco'=> 7.99,
+      'img'=>'cupcake.png'
+    ], 
+    'artesanal'=>[
+      'descricao'=>"	
+      
+      Você escolhe o sabor da massa, recheio, cobertura e o layout do bolo, e nós fazemos a mágica! Fazemos todos os tipos de bolo, seja grande pequeno, simples ou mais elaborado, para aniversário, casamento ou só para matar a vontade!
+      
+      Nossos bolos Gourmet são preparados com chocolate Lindt e outros ingredientes importados.
+      
+      Venha provar nossos deliciosos bolos!
+      
+      Temos bolos a pronta entrega!",
+      'preco'=> 59.99,
+      'img'=>"bolo1.jpeg"
+    ],
+  
+    'brownie'=>[
+      'descricao'=>"Uma delícia. Sobremesa perfeita. Macio e repleto de chocolate, um quadradinho de pura alegria...",
+      'preco'=> 9.99,
+      'img'=>"brownie.jpg"
+    ],
+    
+    
+  ];
+?>
