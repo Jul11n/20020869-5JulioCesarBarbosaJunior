0763d33773cff462f86c77f8833b193bcd150620
jul11n <jcbjr28@gmail.com>
2021-08-23 03:27:29 -0300

    pagina home.php com os produtos

---
+++ pages/home.php
@@ -0,0 +1,41 @@
+<h2>Nossos Produtos</h2>
+
+<div class="produtos">
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/bolo1.jpeg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Bolos Gourmet</h5>
+    <p class="card-text"> 	
+
+Você escolhe o sabor da massa, recheio, cobertura e o layout do bolo, e nós fazemos a mágica!</p>
+    <a href="./index.php?p=artesanal" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/bolo.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Bolos Caseiros</h5>
+    <p class="card-text">Nadinha de misturas prontas ou sabores artificiais, receitas exclusivas, feitas com ingredientes frescos...</p>
+    <a href="./index.php?p=bolos"  class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/brownies/brownie.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Brownies</h5>
+    <p class="card-text">Uma delícia. Sobremesa perfeita. Macio e repleto de chocolate...</p>
+    <a href="./index.php?p=brownies" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/cupcake.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Cupcake</h5>
+    <p class="card-text">Cupcake no tamanho tradicional com massa nos sabores chocolate, baunilha ou cenoura...</p>
+    <a href="./index.php?p=doce" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+</div>
